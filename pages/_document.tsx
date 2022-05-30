96cea44313d42f2b88480be0752ad1fd6c0d76b3
Junsu Min <sunsuking@gmail.com>
2022-05-30 22:27:17 +0900

    PWA 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/img.png" />
+          <meta name="theme-color" content="#fff" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Timer" />
+          <link rel="apple-touch-icon" href="icons/img.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
